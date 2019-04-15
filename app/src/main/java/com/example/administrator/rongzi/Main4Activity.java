d00ab0ce8b2d13f868ff977758586fe2075905d2
dengjiming <dengjiming360@163.com>
2019-04-15 17:43:47 +0800

    enter

---
+++ app/src/main/java/com/example/administrator/rongzi/Main4Activity.java
@@ -0,0 +1,24 @@
+package com.example.administrator.rongzi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class Main4Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+        init();
+
+    }
+
+    private void init() {
+
+
+    }
+}
